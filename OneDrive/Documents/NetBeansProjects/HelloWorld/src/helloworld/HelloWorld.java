2ba4c579cd738ba53cd34f0ef22f0b7d9428b84d
NwizuEmmanuel <nwizuemmanuel200@gmail.com>
2021-03-01 13:09:10 +0100

    # omor java sweetooo obi is aboy

--- OneDrive/Documents/NetBeansProjects/HelloWorld/src/helloworld/HelloWorld.java
+++ OneDrive/Documents/NetBeansProjects/HelloWorld/src/helloworld/HelloWorld.java
@@ -17,7 +17,32 @@ public class HelloWorld {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hello World");
+        int a=20;
+        int b=10;
+        int c=a+b;
+        System.out.println(c+" Ans");
         
+        String HelloMessage;
+        HelloMessage="Hi";
+        System.out.println(HelloMessage);
+        
+        int a1,b1,c1;
+        a1=50;
+        b1=20;
+        c1=a1-b1;
+        System.out.println(c1);
+        
+         String Message = "Hey";
+        System.out.println(Message);
+        
+        String TaxRate = 10;
+        String Subtotal = 7;
+        if (TaxRate > 0){
+            
+            double TaxAmount;
+            TaxAmount = TaxRate * Subtotal;
+            Total = Subtotal + Total;
+        }
     }
     
 }
